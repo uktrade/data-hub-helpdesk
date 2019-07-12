32e819d64069fe1c7b592323068c61e23168569c
Mark Higham <mark.higham@gmail.com>
2019-07-12 12:58:34 +0100

    front-end tooling done

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,27 @@ const del = require('del');
 
 const PROJECT_DIR = path.resolve(__dirname);
 const SASS_FILES = `${PROJECT_DIR}/frontend/sass/**/*.scss`;
-const CSS_DIR = `${PROJECT_DIR}/helpdesk/helpdesk/static/css`;
+const STATIC_DIR = `${PROJECT_DIR}/helpdesk/helpdesk/static`;
+const CSS_DIR = `${STATIC_DIR}/css`;
 const CSS_FILES = `${CSS_DIR}/**/*.css`;
 const CSS_MAPS = `${CSS_DIR}/**/*.css.map`;
-const GOVUK_ASSETS_PATH = `${PROJECT_DIR}/node_modules/govuk-frontend/assets`;
+const GOVUK_PATH = `${PROJECT_DIR}/node_modules/govuk-frontend`;
+const GOVUK_ASSETS_PATH = `${GOVUK_PATH}/assets`;
+const ASSETS_DEST = `${STATIC_DIR}/assets`;
+const JS_DEST = `${STATIC_DIR}/js`;
 
 gulp.task('clean', function() {
   return del([CSS_FILES, CSS_MAPS]);
 });
 
-gulp.task('copy', function() {
+gulp.task('copy-govuk-assets', function() {
   return gulp
     .src([`${GOVUK_ASSETS_PATH}/**/*`], { base: GOVUK_ASSETS_PATH })
-    .pipe(gulp.dest('/tmp'));
+    .pipe(gulp.dest(ASSETS_DEST));
+});
+
+gulp.task('copy-govuk-js', function() {
+  return gulp.src([`${GOVUK_PATH}/all.js`]).pipe(gulp.dest(JS_DEST));
 });
 
 gulp.task('sass:compile', function() {
@@ -42,6 +50,7 @@ gulp.task('sass:watch', function() {
 
 gulp.task('sass', gulp.series('clean', 'sass:compile'));
 
-gulp.task('default', gulp.series('sass'));
-
-
+gulp.task(
+  'default',
+  gulp.series(['sass', 'copy-govuk-assets', 'copy-govuk-js'])
+);
